159e873fa21d628cc31abb5f73ab7bb00b709679
Zeke Burke <48484851+theezedev@users.noreply.github.com>
2025-11-19 17:58:50 -0500

    Changed the logo source and added base vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // ADD THIS LINE TO SET THE BASE PATH FOR GITHUB PAGES:
+  base: "/clkr/", 
+  
   plugins: [react()],
 })
